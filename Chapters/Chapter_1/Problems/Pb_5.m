4a8a6afc4ceb02c5d84c1ef24b2c2ea7ec5d750a
Keyboard Katana <bhonethitkha8@gmail.com>
2025-12-01 22:40:50 +0630

    Add files via upload

---
+++ Chapters/Chapter_1/Problems/Pb_5.m
@@ -0,0 +1,7 @@
+% Sol (a)
+
+sin(0.2*pi)/cos(pi/6)+tand(72)
+
+% Sol (b)
+
+(tand(64)*cosd(15))^2+sind(37)^2/cosd(20)^2
---
+++ Chapters/Chapter_1/Problems/Pb_9.m
@@ -0,0 +1,12 @@
+c = 4.6;
+d = 1.7;
+a = c*d^2;
+b = (c+a)/(c-d);
+
+% Sol (a)
+
+exp(d-b)+(c+a)^(1/3)-(c*a)^d
+
+% Sol (b)
+
+d/c+(c*t/b)^2-c^d-a/b
