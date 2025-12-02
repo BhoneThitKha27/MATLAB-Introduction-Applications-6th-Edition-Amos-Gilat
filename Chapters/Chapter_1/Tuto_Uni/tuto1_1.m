73e4619d2a1c60e2630570a2b547e12d94a3b950
Keyboard Katana <bhonethitkha8@gmail.com>
2025-12-02 12:00:32 +0630

    Add files via upload

---
+++ Chapters/Chapter_1/Tuto_Uni/tuto1_1.m
@@ -0,0 +1,12 @@
+% Sol (a)
+
+x=24;
+LHS=tand(3*x)
+
+RHS=(3*tand(x)-tand(x)^3)/(1-3*tand(x)^2)
+
+% Sol (b)
+
+RHS=cosd(4*x)
+
+LHS=8*(cosd(x)^4-cosd(x)^2)+1
---
+++ Chapters/Chapter_1/Tuto_Uni/tuto1_2.m
@@ -0,0 +1,23 @@
+a = 5.3;
+gamma = 42;
+b = 6;
+
+% Sol for b length
+
+beta = acosd((a^2+c^2-b^2)/(2*a*c))
+
+b1 = sqrt(a^2+c^2-2*a*c*cosd(beta))
+
+% Sol for angles beta and gamma
+
+c = sqrt(a^2+b^2-2*a*b*cosd(gamma))
+
+beta = acosd((a^2+c^2-b^2)/(2*a*c))
+
+gamma1 = acosd((a^2+b^2-c^2)/(2*a*b))
+
+alpha = acosd((b^2+c^2-a^2)/(2*b*c))
+
+% Sol sum of angles
+
+totalAng = alpha+beta+gamma1
